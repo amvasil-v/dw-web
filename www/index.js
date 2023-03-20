cedc8a288734bb0add2c337a91bf46667fcfce5f
Alexander Vasiliev <alexander.vasiliev@siemens.com>
2023-03-20 16:33:27 +0600

    add html page with css style

--- www/index.js
+++ www/index.js
@@ -2,11 +2,42 @@ import { CounterState } from 'dw-web';
 
 const counter_state = CounterState.new();
 
-document.getElementById("counter").textContent = counter_state.get_counter();
+const answer_names = ["button1", "button2", "button3", "button4"];
+const start_button = document.getElementById("start");
+const next_button = document.getElementById("next");
+const answer_label = document.getElementById("answer_label");
 
-document.getElementById("increment").addEventListener("click", () => {
-    document.getElementById("counter").textContent = counter_state.increment_counter();
+function answer_listener(event) {
+	event.target.classList.add("green");
+	next_button.style.visibility = 'visible';
+	for (const btn of answers) {
+	  btn.disabled = true;
+    }
+	answer_label.textContent = "Correct!";
+	answer_label.style.visibility = 'visible';
+    counter_state.increment_counter();
+}
+
+var answers = [];
+for (const name of answer_names) {
+	let button = document.getElementById(name);
+	button.addEventListener("click", answer_listener);
+	answers.push(button);
+}
+
+start_button.addEventListener("click", () => {
+  start_button.style.visibility = 'hidden';
+  answer_label.style.visibility = 'hidden';
+  for (const btn of answers) {
+	  btn.style.visibility = 'visible';
+  }
 });
-document.getElementById("decrement").addEventListener("click", () => {
-    document.getElementById("counter").textContent = counter_state.decrement_counter();
+
+next_button.addEventListener("click", () => {
+  for (const btn of answers) {
+	  btn.classList.remove("green");
+	  btn.disabled = false;
+	  next_button.visibility = 'hidden';
+  }
+  answer_label.style.visibility = 'hidden';
 });
