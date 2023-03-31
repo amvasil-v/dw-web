bcabe51034fde0e2b3fa034ad8c7a14a73b8301b
Evgenii Paikachev <condddr@gmail.com>
2023-03-31 17:08:57 +0400

    refactor frontend

--- www/index.js
+++ www/index.js
@@ -4,86 +4,73 @@ const counter_state = CounterState.new();
 
 init_wasm_logging();
 
-const answer_names = ["button1", "button2", "button3", "button4"];
-const start_button = document.getElementById("start");
-const next_button = document.getElementById("next");
-const answer_label = document.getElementById("answer_label");
-const task_label = document.getElementById("task_label");
+const game = WordsGame.create();
 
-var game = WordsGame.create();
+const start_button = document.getElementById('start');
+const next_button = document.getElementById('next');
+const answer_label = document.getElementById('answer_label');
+const task_label = document.getElementById('task_label');
 
-function set_answers_active(enabled) {
-    for (const btn of answers) {
-        btn.disabled = !enabled;
-    }
-}
+// Init buttons
+const answerButtons = document.querySelectorAll('.btn-answer');
 
-function answer_listener(event) {
-    set_answers_active(false);
-    if (game.check_answer(event.target.answerNumber)) {
-        event.target.classList.add("green");
-        answer_label.textContent = "Correct!";
+const answerClickEvent = (event) => {
+    if (game.check_answer(Number(event.target.dataset.num))) {
+        event.target.classList.add('success');
+        answer_label.textContent = 'Correct!';
     } else {
-        event.target.classList.add("red");
+        event.target.classList.add('danger');
         answer_label.textContent = game.get_incorrent_message();
     }
-    
-    next_button.style.visibility = 'visible';    
+
+    next_button.style.visibility = 'visible';
     answer_label.style.visibility = 'visible';
     counter_state.increment_counter();
-}
+};
 
-var answers = [];
-for (let i = 0; i < 4; i++) {
-    const name = answer_names[i];
-    let button = document.getElementById(name);
-    button.addEventListener("click", answer_listener);
-    button.answerNumber = i;
-    answers.push(button);
-}
+answerButtons.forEach((btn) => btn.addEventListener('click'), answerClickEvent);
 
-function create_exercise() {
+// Init game
+const createExercise = () => {
     if (!game.create_exercise()) {
-        console.error("Failed to create an exercise");
+        console.error('Failed to create an exercise');
         return false;
     }
-    let variants = game.get_answers();
-    answer_label.textContent = "";
+    const variants = game.get_answers();
+    answer_label.textContent = '';
     task_label.textContent = game.get_task();
-    for (let i = 0; i < 4; i++) {
-        answers[i].classList.remove("green");
-        answers[i].classList.remove("red");
-        if (i < variants.length) {
-            answers[i].textContent = variants[i];
-            answers[i].disabled = false;
-        } else {
-            answers[i].textContent = ""
-            answers[i].disabled = true;
-        }
-    }
-    return true;
-}
+    answerButtons.forEach((btn) => {
+        btn.classList.remove('success', 'danger');
+        const num = Number(btn.dataset.num);
+        const variant = variants[num] || '';
+        btn.textContent = variant;
+        btn.disabled = !variant;
+    });
 
-start_button.addEventListener("click", () => {
+    return true;
+};
+
+// Init Controls buttons
+start_button.addEventListener('click', () => {
     try {
-        answer_label.textContent = "Loading...";
+        answer_label.textContent = 'Loading...';
         game.fetch_words().then((res) => {
-            answer_label.textContent = "Words in vocabulary: " + res.toString();
+            answer_label.textContent = 'Words in vocabulary: ' + res.toString();
             if (create_exercise()) {
                 for (const btn of answers) {
                     btn.style.visibility = 'visible';
                 }
                 start_button.style.visibility = 'hidden';
             }
-        })
+        });
     } catch (error) {
-        console.error("Failed to fetch data");
+        console.error('Failed to fetch data');
         console.error(error);
         answer_label.textContent = error.message;
     }
 });
 
-next_button.addEventListener("click", () => {
+next_button.addEventListener('click', () => {
     answer_label.style.visibility = 'hidden';
     if (create_exercise()) {
         next_button.visibility = 'hidden';
